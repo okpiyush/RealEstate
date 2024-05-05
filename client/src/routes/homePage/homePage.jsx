1c93a7e3653aeb1e3740010bdc4aa8d09dd35060
Piyush <piyushkumar2738@gmail.com>
2024-05-05 23:33:16 +0530

    This app is complete now

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+          Welcome to our real estate website! Our experienced and knowledgeable agents are here to guide you through every step of the process, from home staging and photography to mortgage financing and legal advice. Thank you for choosing our real estate website. We look forward to working with you!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
